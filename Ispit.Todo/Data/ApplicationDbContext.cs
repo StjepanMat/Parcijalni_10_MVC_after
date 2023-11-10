196a4610eba980a00f8d2aaa456fec5e759f9ebd
StjepanMat <stjepan.mateljan@gmail.com>
2023-11-10 12:01:12 +0100

    task #3 completed - models, properties, and database migrations  created

--- Ispit.Todo/Data/ApplicationDbContext.cs
+++ Ispit.Todo/Data/ApplicationDbContext.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Ispit.Todo.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Ispit.Todo.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationUser: IdentityUser
+    {
+        [ForeignKey("UserId")]
+        public virtual ICollection<Todolist> Todolist { get; set; }
+    }
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Models.Task> Tasks { get; set; }
+        public DbSet<Todolist> Todolists { get; set; } 
     }
 }
--- Ispit.Todo/Data/Migrations/20231110105019_Initial.Designer.cs
+++ Ispit.Todo/Data/Migrations/20231110105019_Initial.Designer.cs
@@ -4,6 +4,7 @@ using Ispit.Todo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Ispit.Todo.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231110105019_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -113,7 +116,7 @@ namespace Ispit.Todo.Data.Migrations
 
                     b.HasIndex("TodolistId");
 
-                    b.ToTable("Tasks");
+                    b.ToTable("Task");
                 });
 
             modelBuilder.Entity("Ispit.Todo.Models.Todolist", b =>
@@ -140,7 +143,7 @@ namespace Ispit.Todo.Data.Migrations
 
                     b.HasIndex("UserId");
 
-                    b.ToTable("Todolists");
+                    b.ToTable("Todolist");
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRole", b =>
